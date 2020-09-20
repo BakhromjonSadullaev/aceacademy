f27fefae5119cea03e030f3411a0274ac1050380
BakhromjonSadullaev <baxromsadullayev@mail.ru>
2020-09-20 12:05:25 +0500

    typewriting effect

--- app.js
+++ app.js
@@ -165,3 +165,31 @@ function closeAllSelect(elmnt) {
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
+
+
+// TYPING EFFECT STARTS HERE
+const texts = ['ACE ACADEMY !'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+
+    if (count === texts.length) {
+        count = 0;
+    }
+
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+
+    setTimeout(type, 300)
+
+}())
+// TYPING ENDS HERE
